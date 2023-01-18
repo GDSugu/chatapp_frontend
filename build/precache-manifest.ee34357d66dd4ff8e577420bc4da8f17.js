29e2f93b8e7b9ea15b4bffdb2b787d22d3390b50
Sugumar Srinivasan <94180748+GDSugu@users.noreply.github.com>
2023-01-18 21:09:57 +0530

    Add files via upload

---
+++ build/precache-manifest.ee34357d66dd4ff8e577420bc4da8f17.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5a18a739a915023a787f1b6a7deecde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddd1a09f9e5f32915cd4",
+    "url": "/static/css/main.de005c1d.chunk.css"
+  },
+  {
+    "revision": "aa456da483a2127b6253",
+    "url": "/static/js/2.0ff64d94.chunk.js"
+  },
+  {
+    "revision": "bf744d37492ffed4d3003a8f244c622c",
+    "url": "/static/js/2.0ff64d94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddd1a09f9e5f32915cd4",
+    "url": "/static/js/main.e2a4f466.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
